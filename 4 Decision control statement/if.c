be6912036ee74617d804713afa53f9eca3a52471
RISHIKESH NANDAN <rishikesh082005@gmail.com>
2024-04-10 07:47:25 +0530

    if your lience

--- 4 Decision control statement/if.c
+++ 4 Decision control statement/if.c
@@ -4,6 +4,11 @@ int main() {
   int age;
   printf("enter your age: ");
   scanf("%d", &age);
+ 
+  if (age >= 18) {
+    printf("you are eligible for license.\n");
+  }  
+
 
   printf("your age is: %d",age);
 
