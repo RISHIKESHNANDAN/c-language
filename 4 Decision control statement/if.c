3c3078f8f0f635028137b9db8d80324021ee4cff
RISHIKESH NANDAN <rishikesh082005@gmail.com>
2024-04-10 07:11:50 +0530

    if my age

--- 4 Decision control statement/if.c
+++ 4 Decision control statement/if.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 
- int main () {
-    int age;
-    printf("enter your age:");
-    scanf("%d",&age);
+int main() {
+  int age;
+  printf("enter your age: ");
+  scanf("%d", &age);
 
-   printf("your age is:%d",age);
- }
+  printf("your age is: %d",age);
+
+
+  return 0;
+}
