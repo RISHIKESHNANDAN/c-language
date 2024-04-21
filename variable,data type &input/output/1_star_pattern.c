2beebe2ad85935ecec1f31c773f17ce0f59c1c3b
RISHIKESH NANDAN <rishikesh082005@gmail.com>
2024-04-21 08:14:09 +0530

    star pattern question

---
+++ variable,data type &input/output/1_star_pattern.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() {
+     printf("right half pyramid\n");
+
+     printf("*\n");
+      printf("* *\n");
+       printf("* * *\n");
+        printf("* * * *\n");
+         printf("* * * * *\n");
+          printf("* * * * * *\n");
+
+     printf("reverse right half pyramid\n");
+     printf("* * * * * *\n");
+      printf("* * * * *\n");
+       printf("* * * *\n");     
+        printf("* * *\n");  
+         printf("* *\n");
+          printf("*\n");
+
+     printf("left half pyramid\n");
+     printf("    *\n");
+     printf("   **\n");
+     printf("  ***\n");
+     printf("*****\n");         
+}
